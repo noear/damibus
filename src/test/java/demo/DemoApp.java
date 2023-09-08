07e46c72b914564ab4f3fa41126f0f3210f5cc35
noear <noear@live.cn>
2023-09-08 18:11:37 +0800

    0.4

--- src/test/java/demo/DemoApp.java
+++ src/test/java/demo/DemoApp.java
@@ -1,6 +1,8 @@
 package demo;
 
 import org.noear.dami.DamiBus;
+import org.noear.dami.Payload;
+import org.noear.dami.TopicListener;
 
 /**
  * @author noear
@@ -10,37 +12,43 @@ public class DemoApp {
     static String demo_topic = "demo.user.created";
 
     public static void main(String[] args) throws Throwable {
-        listen();
-        send();
+        TopicListener<Payload> listener = createListener();
+
+        //监听
+        DamiBus.global().listen(demo_topic,listener);
+
+        //发送测试
+        sendTest();
+
+        //取消监听
+        DamiBus.global().unlisten(demo_topic,listener);
     }
 
-    //监听
-    private static void listen() {
-        DamiBus.global().listen(demo_topic, payload -> {
+    //创建监听器
+    private static TopicListener<Payload> createListener() {
+        return payload -> {
+            //接收处理
             System.out.println(payload);
-            //接收
+
             if (payload.isRequest()) {
-                //如果是请求载体，则响应一下
+                //如果是请求载体，再响应一下
                 DamiBus.global().response(payload, "你发了：" + payload.getContent());
             }
-        });
+        };
     }
 
-    //发送
-    private static void send() {
+    //发送测试
+    private static void sendTest() {
         //普通发送
         DamiBus.global().send(demo_topic, "{user:'noear'}");
-
 
         //请求并等响应
         String rst1 = DamiBus.global().requestAndResponse(demo_topic, "{user:'dami'}");
         System.out.println("响应返回: " + rst1);
 
-
         //请求并等回调
         DamiBus.global().requestAndCallback(demo_topic, "{user:'solon'}", (rst2) -> {
             System.out.println("响应回调: " + rst2);
         });
-
     }
 }
