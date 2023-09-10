7b07b477a1b9403cf40b1bca9b5143e4d7c6fbb0
noear <noear@live.cn>
2023-09-10 08:07:15 +0800

    优化注释

--- src/test/java/demo_api/mod1/UserEventListenerImpl.java
+++ src/test/java/demo_api/mod1/UserEventListenerImpl.java
@@ -1,6 +1,6 @@
 package demo_api.mod1;
 
-//保持与 Sender 相同的接口定义（参数多一个少一个，倒是可）
+//保持与 Sender 相同的接口定义
 public class UserEventListenerImpl {
     public long created(long userId, String name) {
         System.err.println("created: userId=" + userId + ", name=" + name);
