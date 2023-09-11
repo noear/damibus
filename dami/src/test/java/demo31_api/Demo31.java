ce2ec01fe1ef86a9dabfdacf832e59971534892f
noear <noear@live.cn>
2023-09-11 08:19:44 +0800

    调整示例

--- dami/src/test/java/demo31_api/Demo31.java
+++ dami/src/test/java/demo31_api/Demo31.java
@@ -1,12 +1,12 @@
 package demo31_api;
 
-import demo31_api.module1.UserEventListenerImpl;
+import demo31_api.module1.UserEventListenerOfModule1;
 import demo31_api.module2.UserEventSender;
 import org.noear.dami.Dami;
 
 public class Demo31 {
     public static void main(String[] args) {
-        UserEventListenerImpl userEventListener = new UserEventListenerImpl();
+        UserEventListenerOfModule1 userEventListener = new UserEventListenerOfModule1();
         UserEventSender userEventSender = Dami.api().createSender("demo.user", UserEventSender.class);
 
         //注册监听器
--- dami/src/test/java/demo31_api/module1/UserEventListenerOfModule1.java
+++ dami/src/test/java/demo31_api/module1/UserEventListenerOfModule1.java
@@ -3,7 +3,7 @@ package demo31_api.module1;
 /**
  * 通过约定保持与 Sender 相同的接口定义（或者实现 UserEventSender 接口）
  * */
-public class UserEventListenerImpl {
+public class UserEventListenerOfModule1 {
     public void onCreated(Long userId, String name) {
         System.err.println("onCreated: userId=" + userId + ", name=" + name);
     }
