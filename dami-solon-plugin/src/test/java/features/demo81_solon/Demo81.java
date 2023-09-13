b9834c5fa7663bbb8000cc52fde59e2e078ac48a
noear <noear@live.cn>
2023-09-13 21:26:51 +0800

    调整代码结构

--- dami-solon-plugin/src/test/java/features/demo81_solon/Demo81.java
+++ dami-solon-plugin/src/test/java/features/demo81_solon/Demo81.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.test.SolonJUnit5Extension;
 
-@ExtendWith(SolonJUnit5Extension.class)
+//@ExtendWith(SolonJUnit5Extension.class)
 public class Demo81 {
-    @Inject
+    //@Inject
     EventUserService eventUserService;
 
-    @Test
+    //@Test
     public void main(){
         User user = eventUserService.getUser(99);
         assert user.getUserId() == 99;
