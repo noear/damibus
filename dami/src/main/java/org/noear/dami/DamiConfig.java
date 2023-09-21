9e20948880821b917a2a0fe7af8f67e45ffa738c
noear <noear@live.cn>
2023-09-21 23:40:26 +0800

    添加注释

--- dami/src/main/java/org/noear/dami/DamiConfig.java
+++ dami/src/main/java/org/noear/dami/DamiConfig.java
@@ -10,12 +10,22 @@ import org.noear.dami.bus.DamiBus;
  * @since 1.0
  */
 public class DamiConfig {
+    /**
+     * 配置总线实例
+     *
+     * @param bus 总线实例
+     */
     public static void configure(DamiBus bus) {
         if (bus != null) {
             Dami.bus = bus;
         }
     }
 
+    /**
+     * 配置接口实例
+     *
+     * @param api 接口实例
+     */
     public static void configure(DamiApi api) {
         if (api != null) {
             Dami.api = api;
