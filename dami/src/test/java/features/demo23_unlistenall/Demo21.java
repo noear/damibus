08909ddbca2fe152013839980086f8b810184fe0
Sorghum <sorghum2021@163.com>
2023-10-18 14:43:49 +0800

    feat(DamiBus): unlistenall修改测试用例打印器不同的先后问题.

--- dami/src/test/java/features/demo23_unlistenall/Demo21.java
+++ dami/src/test/java/features/demo23_unlistenall/Demo21.java
@@ -16,36 +16,26 @@ public class Demo21 {
 
     @Test
     public void main() throws InterruptedException {
-        AtomicInteger testObserver = new AtomicInteger();
-
         TopicListener<Payload<String, Long>> aListener = payload -> {
-            System.err.println("i'm a:" + payload);
+            System.out.println("i'm a:" + payload);
         };
-
         TopicListener<Payload<String, Long>> bListener = payload -> {
-            System.err.println("i'm b:" + payload);
+            System.out.println("i'm b:" + payload);
         };
 
         TopicListener<Payload<String, Long>> cListener = payload -> {
-            System.err.println("i'm b:" + payload);
+            System.out.println("i'm b:" + payload);
         };
-
         //监听事件
         bus.listen(topic,aListener);
         bus.listen(topic,bListener);
         bus.listen(topic,cListener);
-
         System.out.println("------------------ 从未移除监听器之前 ------------------");
         //发送事件
         bus.send(topic, "aaaaaa");
-
-        TimeUnit.SECONDS.sleep(1);
-
         System.out.println("------------------ 移除 a 监听器之后 ------------------");
         bus.unlisten(topic,aListener);
         bus.send(topic, "bbbbbb");
-        TimeUnit.SECONDS.sleep(1);
-
         System.out.println("------------------ 移除所有监听器之后 ------------------");
         bus.unlisten(topic);
         bus.send(topic, "cccccc");
