808e7279dab414e5994373a3f0793e0522fb9c87
noear <noear@live.cn>
2025-09-18 20:27:37 +0800

    取消 bus.onCall, bus.onStream，回归 listen

--- dami2/src/test/java/features/demo00/Demo00.java
+++ dami2/src/test/java/features/demo00/Demo00.java
@@ -1,10 +1,46 @@
 package features.demo00;
 
+import org.noear.dami2.Dami;
+import reactor.core.publisher.Flux;
+
 /**
  * @author noear 2023/10/7 created
  */
 public class Demo00 {
-    public void main() {
+    static String topic = "demo.hello";
 
+    public void case_send() {
+        //监听事件
+        Dami.bus().listen(topic, event -> {
+            System.err.println(event.getPayload());
+        });
+
+        //发送事件
+        Dami.bus().send(topic, "hello");
+    }
+
+    public void case_call() throws Exception {
+        //监听事件
+        Dami.bus().listen(topic, (event, content, receiver) -> {
+            System.err.println(event.getPayload());
+            receiver.complete("hi!");
+        });
+
+        //发送事件
+        String rst = Dami.bus().<String, String>call(topic, "hello").get();
+    }
+
+    public void case_stream() {
+        //监听事件
+        Dami.bus().listen(topic, (event, att, content, receiver) -> {
+            System.err.println(event.getPayload());
+            receiver.onNext("hi");
+            receiver.onComplete();
+        });
+
+        //发送事件
+        Flux.from(Dami.bus().stream(topic, "hello")).doOnNext(item -> {
+            System.err.println(item);
+        });
     }
 }
