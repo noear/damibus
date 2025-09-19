7b6facc7e9c3f0e9866f29668eea0430c20d0f26
noear <noear@live.cn>
2025-09-19 16:25:35 +0800

    优化 RoutingTag 性能

--- dami2/src/main/java/org/noear/dami2/bus/route/RoutingTag.java
+++ dami2/src/main/java/org/noear/dami2/bus/route/RoutingTag.java
@@ -27,9 +27,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @since 1.0
  */
 public class RoutingTag<P> extends Routing<P> {
-    private final List<String> tags;
-    private final String topic;
-    private final boolean isPatterned;
+    private final TopicTags topicTags;
 
     /**
      * @param expr     表达式（: 为主题与标签的间隔符；,为标签的间隔符）
@@ -38,21 +36,17 @@ public class RoutingTag<P> extends Routing<P> {
      */
     public RoutingTag(String expr, int index, EventListener<P> listener) {
         super(expr, index, listener);
-        this.isPatterned = expr.indexOf(':') >= 0;
 
-        List<String> exprList = TopicTags.get(expr);
-        this.topic = exprList.get(0);
+        topicTags = TopicTags.get(expr);
+    }
 
-        if (exprList.size() == 1) {
-            this.tags = Collections.emptyList();
-        } else {
-            this.tags = exprList.subList(1, exprList.size());
-        }
+    public String getTopic() {
+        return topicTags.topic;
     }
 
     @Override
     public boolean isPatterned() {
-        return isPatterned;
+        return topicTags.tags.size() > 0;
     }
 
     /**
@@ -65,25 +59,30 @@ public class RoutingTag<P> extends Routing<P> {
             return true;
         }
 
-        List<String> exprList = TopicTags.get(sentTopic);
-        List<String> sendTags = exprList.subList(1, exprList.size());
+        TopicTags sentTopicTags = TopicTags.get(sentTopic);
 
-        boolean topicMatch = exprList.get(0).equals(this.topic);
 
-        boolean notHasTags = tags.isEmpty() || sendTags.isEmpty();
+        boolean topicMatch = topicTags.topic.equals(sentTopicTags.topic);
+        boolean notHasTags = topicTags.tags.isEmpty() || sentTopicTags.tags.isEmpty();
+
         //发送和监听双方都有tags则需要匹配
-        boolean tagMatch = notHasTags || sendTags.stream().anyMatch(this.tags::contains);
+        boolean tagMatch = notHasTags || sentTopicTags.tags.stream().anyMatch(topicTags.tags::contains);
 
         return topicMatch && tagMatch;
     }
 
+
     static class TopicTags {
-        private static final Map<String, List<String>> cached = new ConcurrentHashMap<>();
+        public final Set<String> tags = new HashSet<>();
+        public String topic;
+
+
+        private static final Map<String, TopicTags> cached = new ConcurrentHashMap<>();
 
         /**
          * 获取 TopicTags
          */
-        public static List<String> get(String expr) {
+        public static TopicTags get(String expr) {
             return cached.computeIfAbsent(expr, TopicTags::parse);
         }
 
@@ -91,28 +90,28 @@ public class RoutingTag<P> extends Routing<P> {
          * 解析表达式
          * @return List<String> 第一个元素为topic，剩下的为tag
          */
-        private static List<String> parse(String expr) {
-            List<String> parseList = new ArrayList<>();
+        private static TopicTags parse(String expr) {
+            TopicTags topicTags = new TopicTags();
 
             //处理只写了topic情况
-            if (expr.contains(":") == false) {
-                parseList.add(expr);
-                return parseList;
+            if (expr.indexOf(':') < 0) {
+                topicTags.topic = expr;
+                return topicTags;
+            } else{
+                //topic:tag1,tag2,tag3 以 : 切割为两部分
+                String[] topicSplit = expr.split(":");
+                topicTags.topic =  topicSplit[0];
+
+                //处理"topic:"情况
+                if (topicSplit.length == 1) {
+                    return topicTags;
+                }else {
+                    //tag1,tag2,tag3 以 , 切割数组作为tags
+                    String[] tagSplit = topicSplit[1].split(",");
+                    topicTags.tags.addAll(Arrays.asList(tagSplit));
+                    return topicTags;
+                }
             }
-
-            //topic:tag1,tag2,tag3 以 : 切割为两部分
-            String[] topicSplit = expr.split(":", 2);
-            parseList.add(topicSplit[0]);
-
-            //处理"topic:"情况
-            if (topicSplit[1].isEmpty()) {
-                return parseList;
-            }
-            //tag1,tag2,tag3 以 , 切割数组作为tags
-            String[] tagSplit = topicSplit[1].split(",");
-            parseList.addAll(Arrays.asList(tagSplit));
-
-            return parseList;
         }
     }
 }
