913dab0f4aff3e88f1969c70cf5c099cdd6896d1
noear <noear@live.cn>
2025-09-19 13:48:35 +0800

    添加新的测试代码

---
+++ dami2/src/test/java/benchmark/RouterTest.java
@@ -0,0 +1,28 @@
+package benchmark;
+
+import org.noear.dami2.Dami;
+import org.noear.dami2.DamiConfig;
+import org.noear.dami2.bus.route.EventRouterPatterned;
+import org.noear.dami2.bus.route.RoutingPath;
+
+/**
+ * @author noear 2025/9/19 created
+ */
+public class RouterTest {
+    static int count = 0;
+
+    public static void main(String[] args) {
+        DamiConfig.configure(new EventRouterPatterned(RoutingPath::new));
+
+        Dami.bus().listen("test.demo", e -> {
+            count = count + 1;
+        });
+
+        //不预热，直接开始
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000_000; i++) {
+            Dami.bus().send("test.demo", "1");
+        }
+        System.out.println(System.currentTimeMillis() - start + "::" + count);
+    }
+}
